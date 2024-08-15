4e8433e49883978cbb7907be61bd9bd326d8cb4e
SoliBlack <SolitaireBlack21@gmail.com>
2024-08-14 21:10:28 -0400

    Updated the google links

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ export default [
     {
         title: "Mount Fuji",
         location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://www.google.com/maps/place/Mount+Fuji/@35.4712478,137.9956136,9z/data=!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
@@ -19,7 +19,7 @@ export default [
     {
         title: "Waikiki Beach",
         location: "Oahu, Hawaii, USA",
-        googleMapsUrl: "https://www.google.com/maps/place/Oahu,+Hawaii,+USA",
+        googleMapsUrl: "https://www.google.com/maps/place/Waikiki,+Honolulu,+HI+96815/@21.2801697,-157.8533409,14z/data=!3m1!4b1!4m6!3m5!1s0x7c006df4e5c129af:0x4633ddc52a688878!8m2!3d21.2793462!4d-157.8291847!16zL20vMGc3Z2Y?entry=ttu",
         startDate: "May 1, 2022",
         endDate: "May 15, 2022",
         description: "Waikiki Beach is famous for its golden sands, turquoise waters, and vibrant atmosphere, offering perfect conditions for surfing, sunbathing, and enjoying stunning sunsets.",
@@ -28,7 +28,7 @@ export default [
     {
         title: "El Nido",
         location: "Philippines",
-        googleMapsUrl: "https://www.google.com/maps/place/Philippines",
+        googleMapsUrl: "https://www.google.com/maps/place/El+Nido,+Palawan,+Philippines/@11.2052325,119.075716,10z/data=!3m1!4b1!4m6!3m5!1s0x33b65515ef5c9e0f:0xfc4e665b599b0455!8m2!3d11.1953602!4d119.4189097!16zL20vMDJodjVr?entry=ttu",
         startDate: "March 10, 2023",
         endDate: "March 25, 2023",
         description: "El Nido, Palawan, is a tropical paradise known for its limestone cliffs, turquoise waters, and pristine beaches. It's an ideal spot for both adventure and relaxation.",
@@ -37,7 +37,7 @@ export default [
     {
         title: "Garden of the Gods",
         location: "Colorado, USA",
-        googleMapsUrl: "https://www.google.com/maps/place/Colorado,+USA",
+        googleMapsUrl: "https://www.google.com/maps/search/garden+of+the+gods/@38.8690936,-104.942181,12z?entry=ttu",
         startDate: "August 5, 2021",
         endDate: "August 20, 2021",
         description: "Garden of the Gods in Colorado Springs is renowned for its dramatic red sandstone rock formations set against the backdrop of the Rocky Mountains, offering breathtaking views and excellent hiking opportunities.",
@@ -46,7 +46,7 @@ export default [
     {
         title: "Luray Caverns",
         location: "Virginia, USA",
-        googleMapsUrl: "https://www.google.com/maps/place/Virginia,+USA",
+        googleMapsUrl: "https://www.google.com/maps/place/Luray+Caverns/@38.6645508,-78.5016427,15z/data=!3m1!4b1!4m6!3m5!1s0x89b4518a553d7ead:0xe192850029bd4fbd!8m2!3d38.6645355!4d-78.4832102!16zL20vMDNkbjB5?entry=ttu",
         startDate: "September 10, 2020",
         endDate: "September 25, 2020",
         description: "Luray Caverns is a stunning underground wonder in Virginia, with intricate rock formations beautifully illuminated. It's a must-see for those fascinated by natural beauty.",
@@ -55,7 +55,7 @@ export default [
     {
         title: "Vicksburg National Military Park",
         location: "Mississippi, USA",
-        googleMapsUrl: "https://www.google.com/maps/place/Mississippi,+USA",
+        googleMapsUrl: "https://www.google.com/maps/place/Vicksburg+National+Military+Park/@32.3548577,-90.8478391,17z/data=!3m1!4b1!4m6!3m5!1s0x8628e1dbb2d1e605:0x47a0df70bc77d383!8m2!3d32.3548532!4d-90.8452588!16zL20vMDV5ZzVu?entry=ttuhttps://www.google.com/maps/place/Mississippi,+USA",
         startDate: "June 15, 2019",
         endDate: "June 30, 2019",
         description: "Vicksburg National Military Park preserves Civil War history with rolling hills, monuments, and cannons. It's a peaceful site for reflection on America's past.",
